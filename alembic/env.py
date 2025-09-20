575860d64bc8d350bba36937178be44dcbd2c702
Wenzo <rithellyenzo@gmail.com>
2025-09-20 16:49:57 -0300

    Add initial tables

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,9 @@ from sqlalchemy import pool
 
 from alembic import context
 from app.core.config import settings
-from app.entities.sample.models.sample_models import SampleModels
+from app.entities.models.prompts import PromptModel
+from app.entities.models.chat_messages import ChatMessageModel
+from app.entities.models.chat_sessions import ChatSessionModel
 from app.clients.db.postgres_client import Base
 
 # this is the Alembic Config object, which provides
@@ -74,9 +76,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
